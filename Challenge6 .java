318f84ba99021e7c0fec6324994c0e49a4915578
Kyle Turner <kylet2014@outlook.com>
2016-09-26 14:26:57 +0100

    Add files via upload

---
+++ Challenge6 .java
@@ -0,0 +1,41 @@
+
+package challenge.pkg4;
+
+import java.util.Scanner;
+
+public class Challenge4 {
+
+
+    public static void main(String[] args) {
+    
+Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter Height");
+        int num1 = myScanner.nextInt();
+        System.out.println("Enter Length");
+        int num2 = myScanner.nextInt();
+        int num3 = num1 * num2;
+        System.out.println("The area of the rectangle is " + num3);
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+    }
+    
+}
